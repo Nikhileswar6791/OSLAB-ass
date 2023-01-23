298d3373a18b3b955760e611193fb448cf864fae
Nikhileswar6791 <nikhileswarreddypochimireddy@gmail.com>
2023-01-23 03:31:49 +0000

    changed schedule.c

--- Lab2- Part1/schedule.c
+++ Lab2- Part1/schedule.c
@@ -325,7 +325,7 @@ static int schedule_process(struct schedproc * rmp, unsigned flags)
 		rmp->endpoint, err);
 	}
 	if (rmp->priority >= USER_Q){
-		printf("PID %d swapped in\n", _ENDPOINT_P(rmp->endpoint));
+		printf("200010040_200010006 PID %d swapped in\n", _ENDPOINT_P(rmp->endpoint));
 	}
 
 	return err;
